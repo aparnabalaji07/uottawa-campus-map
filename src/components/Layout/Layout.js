b763a86d79f658be74a36cd5230729917fdf83f2
aparnabalaji07 <aparna.balaji@gmail.com>
2026-01-17 22:54:00 -0500

    fixed Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,26 +1,19 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import CampusNavigation from '@components/CampusNavigation';
 import styles from './Layout.module.scss';
 
-export default function NavigationPage() {
-  return (
-    <Layout>
-      <CampusNavigation />
-    </Layout>
-  );
-};
-
-const Layout = ({ children }) => {
+export default function Layout({ children }) {
   return (
     <div className={styles.layout}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.main}>{children}</main>
+      <main className={styles.main}>
+        {children}
+      </main>
       <Footer />
     </div>
   );
-};
+}
